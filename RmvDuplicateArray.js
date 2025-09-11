09a6c550ce042f1aac7d8e2599fc138e9a7806ac
Sumant sahoo <163635318+Sumant246u@users.noreply.github.com>
2025-09-11 18:36:02 +0530

    Update RmvDuplicateArray.js
    Add other way

--- RmvDuplicateArray.js
+++ RmvDuplicateArray.js
@@ -7,3 +7,23 @@ for (let i = 0; i < arr.length; i++) {
     
 }
 console.log(unique);
+
+// Using forEach()
+let numbers = [1, 2, 2, 3, 4, 4, 5];
+let unique = [];
+numbers.forEach((num) => {
+  if (!unique.includes(num)) {
+    unique.push(num);
+  }
+});
+console.log(unique); // [1, 2, 3, 4, 5]
+
+// Using reduce()
+let numbers = [1, 2, 2, 3, 4, 4, 5];
+let unique = numbers.reduce((acc, curr) => {
+  if (!acc.includes(curr)) {
+    acc.push(curr);
+  }
+  return acc;
+}, []);
+console.log(unique); // [1, 2, 3, 4, 5]
